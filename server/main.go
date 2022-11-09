e3ccd663a7a72dfe67d9017a03496fd943ddc5ca
Bruno Teixeira <brunoalexandre3@hotmail.com>
2022-11-09 21:05:18 +0000

    Updated some TODOS

--- server/main.go
+++ server/main.go
@@ -25,10 +25,18 @@ type Client struct {
 
 //TODO:
 // CHECK EXCALIDRAW DRAWING
-// - Verify that this is a new Client
-// - Add this client to a slice of clients
-// - After all this, send confirmation to client socket
-// - Client receive this confirmation and keeps listening on his socket
+// - Register logic (Go routine that handles incoming registers and exits on Ctrl+C (create a channel ?))
+    // - Verify that this is a new Client
+    // - Add this client to a slice of clients
+    // - After all this, send confirmation to client socket
+    // - Client receive this confirmation and keeps listening on his socket
+
+// - Go routine that handles stdin to send comands to clients from server
+// - Create log folder
+    // - Create file that logs registers and exists
+    // - Create file that logs commands per client with command + response (each client has a command_log.log file)
+
+// - Implement command encription on server and command decription on client with one time key given on c2 server startup
 
 func handleConnection(conn net.Conn) {
     dec := gob.NewDecoder(conn)
