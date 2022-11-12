482b9bc1a9dfc233440b2169fda0cf0196306c71
BrunoTeixeira1996 <brunoalexandre3@hotmail.com>
2022-11-12 13:11:35 +0000

    Register logic almost done

--- client/main.go
+++ client/main.go
@@ -7,15 +7,13 @@ import (
     "net"
     "os"
     "runtime"
-
     "github.com/google/uuid"
 )
 
-// Application constants, defining host, port, and protocol.
 const (
-    connHost = "localhost"
-    connPort = "9988"
-    connType = "tcp"
+    SERVER_HOST = "localhost"
+    SERVER_PORT = "9988"
+    SERVER_TYPE = "tcp"
 )
 
 type Client struct {
@@ -23,6 +21,8 @@ type Client struct {
     Hostname string
     Os       string
     Arch     string
+    IP       string
+    Port     string
 }
 
 func (cli *Client) generateRandomId() {
@@ -40,30 +40,80 @@ func (cli *Client) getHostName() {
     cli.Hostname, _ = os.Hostname()
 }
 
-// Function that handles the errors
-func run() error {
-    log.Println("Connecting to", connType, "server", connHost+":"+connPort)
-    conn, err := net.Dial(connType, connHost+":"+connPort)
-
-    if err != nil {
-        fmt.Println("Error connecting:", err.Error())
-        os.Exit(1)
-    }
-
-    encoder := gob.NewEncoder(conn)
-    client := &Client{Id: "1"}
+func createNewCli() Client{
+    // TODO: create random ports for now to work with multiple clients
+    client := Client{IP: "localhost", Port: "9000"}
+    client.generateRandomId()
     client.getClientOsAndArch()
     client.getHostName()
+
+    return client
+}
+
+// Function to register on server socket
+func registerOnServerSocket() (Client, error) {
+    conn, err := net.Dial(SERVER_TYPE, SERVER_HOST+":"+SERVER_PORT)
+    if err != nil {
+        log.Println("Error connecting:", err.Error())
+        return Client{}, err
+    }
+    log.Println("Connected with server")
+
+    // Send client struct to register in server socket
+    encoder := gob.NewEncoder(conn)
+    client := createNewCli()
     encoder.Encode(client)
 
-    // for {
-    //     // TODO: create go routine that sends hearthbeat to server
-    // }
+    return client, nil
+}
+
+// Function that verifies if client was registered
+func verifyRegister(client Client)  error {
+    // Get info from server to verify if we are registered
+    clientSocket, err := net.Listen("tcp", client.IP+":"+client.Port)
+    if err != nil {
+        log.Println("Error listening:", err.Error())
+        return err
+    }
+
+    defer clientSocket.Close()
+
+    fmt.Println("Listening on client socket")
+
+    connection, err := clientSocket.Accept()
+    if err != nil {
+        log.Println("Error accepting server message:", err.Error())
+        return err
+    }
+    
+    serverMsg := make([]byte, 1024)
+    serverMsgLen, err := connection.Read(serverMsg)
+
+    // TODO: make a new register message
+    if string(serverMsg[:serverMsgLen]) != "REGISTERED" {
+        return fmt.Errorf("Something went wrong and the client was not registered by server")
+    }
 
     return nil
 }
 
-func main() {)
+// Function that handles the errors
+func run() error {
+    // Registers on server socket
+    client, err := registerOnServerSocket()
+    if  err != nil {
+        return err
+    }
+
+    // Verifies if the register was well done
+    if err := verifyRegister(client); err != nil {
+        return err
+    }
+   
+   return nil
+}
+
+func main() {
     if err := run(); err != nil {
         fmt.Fprint(os.Stderr, err)
         os.Exit(1)
--- server/main.go
+++ server/main.go
@@ -15,20 +15,20 @@ const (
 )
 
 type Client struct {
-    Id string
-    Hostname string
-    Os string
-    Arch string
-    // IP
-    // PORT
+    Id         string
+    Hostname   string
+    Os         string
+    Arch       string
+    IP         string
+    Port       string
 }
 
 //TODO:
-// CHECK EXCALIDRAW DRAWING
-// - Register logic (Go routine that handles incoming registers and exits on Ctrl+C (create a channel ?))
-    // - Verify that this is a new Client
-    // - Add this client to a slice of clients
-    // - After all this, send confirmation to client socket
+// - Register logic (Go routine that handles incoming registers and exits on Ctrl+C
+    // - Verify that this is a new Client (DONE)
+    // - Add this client to a slice of client (DONE)
+    // - After all this, send confirmation to client socket (DONE)
+    // - Work with multiple clients to test this (Need to randomize ports on client.go)
     // - Client receive this confirmation and keeps listening on his socket
 
 // - Go routine that handles stdin to send comands to clients from server
@@ -36,6 +36,7 @@ type Client struct {
     // - Create file that logs registers and exists
     // - Create file that logs commands per client with command + response (each client has a command_log.log file)
 
+// - Implement mutexes to control goroutines
 // - Implement command encription on server and command decription on client with one time key given on c2 server startup
 
 func handleConnection(conn net.Conn) {
@@ -48,10 +49,33 @@ func handleConnection(conn net.Conn) {
     conn.Close()
 }
 
-// Function that handles the errors
-func run() error {
-    fmt.Println("Server Running ... ")
+func handleRegister(conn net.Conn, clientsSlice *[]Client) {
+    dec := gob.NewDecoder(conn)
+    client := &Client{}
+    dec.Decode(client)
 
+    for _, registeredClient := range *clientsSlice{
+        if client.Id == registeredClient.Id {
+            fmt.Println("This client already exists")
+            conn.Close()
+        }
+    }
+
+    *clientsSlice = append(*clientsSlice, *client)
+    fmt.Printf("Client %s added to slice\n", client.Id)
+    
+
+    // Sends confirmation to client socket
+    clientConn, err := net.Dial("tcp", client.IP+":"+client.Port)
+    if err != nil {
+        log.Println("Error connecting to client socket:", err.Error())
+    } else {
+        log.Println("Connected with client")
+        clientConn.Write([]byte("REGISTERED"))
+    }
+}
+
+func startRegisterSocket() error {
     server, err := net.Listen(SERVER_TYPE, SERVER_HOST+":"+SERVER_PORT)
     if err != nil {
         log.Println("Error listening:", err.Error())
@@ -61,7 +85,8 @@ func run() error {
     defer server.Close()
 
     fmt.Println("Listening on " + SERVER_HOST + ":" + SERVER_PORT)
-    fmt.Println("Waiting for client...")
+
+    clientsSlice := &[]Client{}
 
     for {
         connection, err := server.Accept()
@@ -71,9 +96,19 @@ func run() error {
             return err
         }
 
-        fmt.Println("Client connected")
-        go handleConnection(connection)
+        go handleRegister(connection, clientsSlice)
     }
+}
+
+// Function that handles the errors
+func run() error {
+    // Starts server register socket
+    err := startRegisterSocket()
+    if err != nil {
+        return err
+    }
+
+    return nil
 }
 
 func main() {
